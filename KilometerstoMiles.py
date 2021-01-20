840498e29f366d42eaa3cc1c96bf82616e8ba005
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-21 00:17:51 +0530

    commit

--- KilometerstoMiles.py
+++ KilometerstoMiles.py
@@ -1,3 +1,8 @@
 # Python Program to Convert Kilometers to Miles 
 
+km = float(input("Enter value in kilometers: "))
 
+# 1km = 0.621371 miles
+conv_fac = 0.621371
+miles = km * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(km,miles))
