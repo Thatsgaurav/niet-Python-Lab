6208d362776206d79a057bc21ae521e7a500bc4b
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-21 23:02:33 +0530

    commit

--- rightangledtriangle.py
+++ rightangledtriangle.py
@@ -1,2 +1,9 @@
 # Python program to calculate the hypotenuse of a right angled triangle. 
 
+import math
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+result = math.sqrt((a ** 2) + (b ** 2))
+print("Hypotenuse:", result)
