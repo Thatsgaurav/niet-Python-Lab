e486e019f936582fcdeeb3176c167ec5fb57aa9f
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-22 19:53:10 +0530

    commit

--- yesterdaytodaytomorrow.py
+++ yesterdaytodaytomorrow.py
@@ -1,1 +1,9 @@
 # Python program to print yesterday, today, tomorrow.  
+
+import datetime 
+today = datetime.date.today()
+yesterday = today - datetime.timedelta(days = 1)
+tomorrow = today + datetime.timedelta(days = 1) 
+print('Yesterday : ',yesterday)
+print('Today : ',today)
+print('Tomorrow : ',tomorrow)
