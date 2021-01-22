ec467f2f68abd48dbccdb521f404275de14a07cd
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-22 20:47:56 +0530

    commit

--- name_age_address_in_three_different_lines.py
+++ name_age_address_in_three_different_lines.py
@@ -1,1 +1,14 @@
 # Python program to display your details like name, age, address in three different lines
+
+def personal_details():
+    name, age = "Gaurav", 19
+    address = "Delhi, India"
+
+    # User Input
+    # name = str(input("Enter your name :"))
+    # age = int(input("Enter your age :"))
+    # address = str(input("Enter your address :"))
+    
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+personal_details()
