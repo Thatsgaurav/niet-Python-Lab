3d718c63cae3d8ffa9459be866872f48d9d785a2
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-22 14:44:11 +0530

    commit

--- rightangledtriangle.py
+++ rightangledtriangle.py
@@ -7,3 +7,16 @@ b = float(input("Enter second number: "))
 
 result = math.sqrt((a ** 2) + (b ** 2))
 print("Hypotenuse:", result)
+
+# Another Type 
+# import math
+
+# def main():
+#     side1 = int(input("Enter side1 :"))
+#     side2 = int(input("Enter side2 :"))
+#     hypotenuse = math.sqrt(math.pow(side1, 2)+math.pow(side2, 2))
+#     print("HYPOTENUSE = "+ str(hypotenuse))
+    
+    
+# if __name__ == '__main__':
+#     main()
