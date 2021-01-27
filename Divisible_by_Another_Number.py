e3a23a916b7a74eec500d089d20af37419348300
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-27 18:45:38 +0530

    commit

--- Divisible_by_Another_Number.py
+++ Divisible_by_Another_Number.py
@@ -1,1 +1,9 @@
 # Python Program to Find Numbers Divisible by Another Number
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+remainder = num1 % num2
+if remainder == 0:
+    print(num1,"is divisible by",num2)
+else:
+    print(num1,"is not divisible by", num2)    
