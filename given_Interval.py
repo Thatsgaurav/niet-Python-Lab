71d144f303ffcde34c4e98e70d3302c268656ee6
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-27 23:33:04 +0530

    commit

--- given_Interval.py
+++ given_Interval.py
@@ -1,1 +1,15 @@
 # Python Program to Print all Prime Numbers in a given Interval
+
+lower = 0
+upper = 100
+
+print("Prime numbers in interval", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
