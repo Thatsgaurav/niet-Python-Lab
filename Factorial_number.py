0702ce6d287487c59ca7d84c6f26a39f2c6c7cfb
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-28 11:11:08 +0530

    commit

--- Factorial_number.py
+++ Factorial_number.py
@@ -1,1 +1,12 @@
 # Python Program to Find the Factorial of a given number
+
+num = int(input("Enter a number: "))
+factorial = 1
+if num < 0:
+   print()
+elif num == 0:
+   print()
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("factorial of",num,"is",factorial)
