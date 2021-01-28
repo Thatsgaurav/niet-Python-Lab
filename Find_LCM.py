3e4951469e79c96971b547e49dc74858d0a2e364
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-28 11:38:38 +0530

    commit

--- Find_LCM.py
+++ Find_LCM.py
@@ -1,1 +1,24 @@
 # Python Program to Find LCM of two numbers.
+
+def compute_lcm(x, y):
+
+   if x > y:
+       greater = x
+   else:
+       greater = y
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+# num1 = 0
+# num2 = 0
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter sec number: "))
+
+
+print("The L.C.M. is", compute_lcm(num1, num2))
