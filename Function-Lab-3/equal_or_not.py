c8aae1825739d76f813c90ecca07699dedc7386b
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-18 13:48:02 +0530

    commit

--- Function-Lab-3/equal_or_not.py
+++ Function-Lab-3/equal_or_not.py
@@ -1,1 +1,19 @@
 # WAP to check whether the two numbers are equal or not. 
+
+def equal_or_not(a,b):
+    if(a==b):
+        return 0
+    if(a>b):
+        return 1
+    if(a<b):
+        return -1
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b: "))
+result = equal_or_not(a,b)
+if (result==0):
+    print("a is equal to b")
+if (result==1):
+    print("a is greater than b")
+if (result==-1):
+    print("a is less than b")        
+        
