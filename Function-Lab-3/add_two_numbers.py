4bc990d02a0dbe259874d6ac41d671da6c9460d3
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-18 11:53:31 +0530

    commit

--- Function-Lab-3/add_two_numbers.py
+++ Function-Lab-3/add_two_numbers.py
@@ -1,1 +1,9 @@
 # WAP to implement the user defined function to add two numbers. 
+
+def add_num(a,b):
+    sum=a+b
+    return sum
+num1=int(input("Enter First number: "))
+num2=int(input("Enter Second number: "))
+
+print("sum is",add_num(num1,num2))
