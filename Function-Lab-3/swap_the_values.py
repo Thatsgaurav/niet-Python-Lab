67e114b663c88e4c11337b63e8f2d6489dfe0a93
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-18 21:57:00 +0530

    commit

--- Function-Lab-3/swap_the_values.py
+++ Function-Lab-3/swap_the_values.py
@@ -1,1 +1,14 @@
 # WAP to swap the values of two variable that are defined as global variables. 
+
+def swap(x,y):
+    x = x + y 
+    y = x - y
+    x = x - y
+    print("x after swap = ", x)
+    print("y after swap = ", y)
+x = int(input("Enter value of x: "))
+y = int(input("Enter value of y: "))
+swap(x,y)
+
+
+# Working on this 
