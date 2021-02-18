f36779c66f2c41802d844e83b14d881b50c1ee57
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-18 16:22:48 +0530

    commit

--- Function-Lab-3/leap_year_or_not.py
+++ Function-Lab-3/leap_year_or_not.py
@@ -1,1 +1,19 @@
-# 
+# WAP to check whether the given year is a leap year or not using user defined function. 
+
+def checkYear(year):
+	if (year % 4) == 0:
+		if (year % 100) == 0:
+			if (year % 400) == 0:
+				return True
+			else:
+				return False
+		else:
+			return True
+	else:
+		return False
+year = int(input("Enter Year: "))
+if(checkYear(year)):
+	print("Leap Year")
+else:
+	print("Not a Leap Year")
+
