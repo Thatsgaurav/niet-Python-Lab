39d171a15fe8aa3dd8684dd5e612f265be73036b
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-19 13:43:15 +0530

    commit

--- Function-Lab-3/display_the_pattern.py
+++ Function-Lab-3/display_the_pattern.py
@@ -1,1 +1,23 @@
 # WAP to display the pattern using user defined function. 
+
+def pypart(n):
+	for i in range(0, n):
+		for j in range(0, i+1):
+			print("* ",end="")
+		print("\r")
+n = 5
+pypart(n)
+
+#  pypart2 
+
+def pypart2(n):
+    k = 2*n - 2
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 2
+        for j in range(0, i+1):
+            print("* ", end="")
+        print("\r")
+n = 5
+pypart2(n)
