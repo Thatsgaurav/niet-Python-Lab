b6a1d4b86ca369f892cccc689284a522e3c528c3
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-19 17:59:40 +0530

    commit

---
+++ Function-Lab-3/sum_of_digits_recursion.py
@@ -0,0 +1,10 @@
+# WAP to compute the sum of digits of the given number using recursion. 
+
+def sum_of_digit( n ): 
+    if n == 0: 
+        return 0
+    return (n % 10 + sum_of_digit(int(n / 10))) 
+  
+num = int(input("Enter a number: "))
+result = sum_of_digit(num) 
+print("Sum of digits in",num,"is", result)
