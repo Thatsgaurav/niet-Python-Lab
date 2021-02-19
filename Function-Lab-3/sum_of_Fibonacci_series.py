c2de650a25021430f3dcd615d40a534e91a4589e
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-19 14:45:31 +0530

    commit

--- Function-Lab-3/sum_of_Fibonacci_series.py
+++ Function-Lab-3/sum_of_Fibonacci_series.py
@@ -1,1 +1,18 @@
 # WAP to compute the sum of Fibonacci series up to nth term using user defined function. 
+
+def calculateSum(n) : 
+	if (n <= 0) : 
+		return 0
+
+	fibo =[0] * (n+1) 
+	fibo[1] = 1
+	sm = fibo[0] + fibo[1] 
+	for i in range(2,n+1) : 
+		fibo[i] = fibo[i-1] + fibo[i-2] 
+		sm = sm + fibo[i] 
+		
+	return sm 
+n = int(input("Enter value of n: "))
+print("Sum of Fibonacci numbers is : " , 
+	calculateSum(n)) 
+
