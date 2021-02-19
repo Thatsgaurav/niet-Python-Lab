ae79b4ecd73cef2cd7d85008ce3699787c413be8
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-19 13:20:54 +0530

    commit

--- Function-Lab-3/sum_of_the_series.py
+++ Function-Lab-3/sum_of_the_series.py
@@ -1,1 +1,15 @@
 # WAP compute the sum of the series using user defined function. 
+
+def fact(n):
+    f = 1 
+    if(n==0 or n==1):
+        return 1 
+    else:
+        for i in range(1,int(n+1)):
+            f = f*i
+    return f
+n = int(input("Enter the value of n: "))
+s = 0.0
+for i in range(1,n+1):
+    s = s+(float(i**i)/fact(i))
+print("Result :",s)                      
