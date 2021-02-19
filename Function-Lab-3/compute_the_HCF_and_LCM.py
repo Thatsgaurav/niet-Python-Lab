85fce961fdd55cf2c2a145b0762d0d84b414f3c4
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-19 10:48:49 +0530

    commit

--- Function-Lab-3/compute_the_HCF_and_LCM.py
+++ Function-Lab-3/compute_the_HCF_and_LCM.py
@@ -1,1 +1,32 @@
 # WAP to compute the HCF and LCM of two numbers using user defined function. 
+
+# HCF
+  
+def hcfnaive(a,b): 
+	if(b==0): 
+		return a 
+	else: 
+		return hcfnaive(b,a%b) 
+
+a = int(input("Enter value a: "))
+b= int(input("Enter value b: "))
+ 
+print ("The hcf of a and b is : ",end="") 
+print (hcfnaive(a,b)) 
+
+hcfnaive(a,b)
+
+# LCM
+
+def gcd(a,b): 
+	if a == 0: 
+		return b 
+	return gcd(b % a, a) 
+
+def lcm(a,b): 
+	return (a / gcd(a,b))* b 
+ 
+a = int(input("Enter value a: "))
+b = int(input("Enter value b: "))
+print('LCM of', a, 'and', b, 'is', lcm(a, b)) 
+
