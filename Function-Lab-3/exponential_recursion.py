c02bdb61fcf647a6144f55f81feae1d54ef56fa2
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-19 17:55:59 +0530

    commit

--- Function-Lab-3/exponential_recursion.py
+++ Function-Lab-3/exponential_recursion.py
@@ -1,1 +1,10 @@
 # WAP to compute the exponential of number w.r.t. another number using recursion.  
+
+def power(base,exp):
+    if(exp==1):
+        return(base)
+    if(exp!=1):
+        return(base*power(base,exp-1))
+base=int(input("Enter base: "))
+exp=int(input("Enter exponential value: "))
+print("Result:",power(base,exp))
