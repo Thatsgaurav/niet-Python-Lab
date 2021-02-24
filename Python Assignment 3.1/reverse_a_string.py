261aab590d3d95e314d132ef4c11b05ce8000bd6
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-24 20:12:53 +0530

    commit

--- Python Assignment 3.1/reverse_a_string.py
+++ Python Assignment 3.1/reverse_a_string.py
@@ -1,19 +1,30 @@
 # Write a Python function to reverse a string
 # Using recursion
 
-def reverse(s):
-    if len(s) == 0:
-        return s
-    else:
-        return reverse(s[1:]) + s[0]
+def reverse(str):
+    new_str = ''
+    i = len(str)-1
+    while i>=0:
+        new_str += str[i]
+        i -= 1
+    return new_str
+str = input("Enter a string : ")
+print("The reversed string is : ", reverse(str))
 
-s = input("Enter String: ")
 
-print("The original string  is : ", end="")
-print(s)
-
-print("The reversed string is : ", end="")
-print(reverse(s))
+# def reverse(s):
+#     if len(s) == 0:
+#         return s
+#     else:
+#         return reverse(s[1:]) + s[0]
+#
+# s = input("Enter String: ")
+#
+# print("The original string  is : ", end="")
+# print(s)
+#
+# print("The reversed string is : ", end="")
+# print(reverse(s))
 
 
 # Using Loop
