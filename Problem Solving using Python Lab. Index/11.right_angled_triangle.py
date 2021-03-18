8825677722d55dd9b85246bcf6b17c94d032fcc9
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-03-18 02:32:34 -0500

    commit

--- Problem Solving using Python Lab. Index/11.right_angled_triangle.py
+++ Problem Solving using Python Lab. Index/11.right_angled_triangle.py
@@ -6,5 +6,5 @@ import math
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 
-result = math.sqrt((a ** 2) + (b ** 2))
-print("Hypotenuse:", result)
+h = math.sqrt(a**2 + b**2)
+print("Hypotenuse is:", h)
