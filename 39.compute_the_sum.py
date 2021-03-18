16c1560a575ed8f0f439e35f79f5954b8db1ba5f
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-03-18 13:53:27 -0500

    commit

--- 39.compute_the_sum.py
+++ 39.compute_the_sum.py
@@ -1,1 +1,8 @@
 # . WAP to compute the sum of following series up to the nth term.1 + 𝑥ଵ/1! + 𝑥ଶ/2! +𝑥 ଷ/3! + ……. 
+
+n=int(input("Enter the number of terms:"))
+x=int(input("Enter the value of x:"))
+sum1=1
+for i in range(2,n+1):
+    sum1=sum1+((x**i)/i)
+print("The sum of series is",round(sum1,2))
