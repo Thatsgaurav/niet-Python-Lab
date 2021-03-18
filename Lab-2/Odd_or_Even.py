7e197f393d2ee802ae508c425dd66dcb98ae0fbb
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-03-18 09:34:05 -0500

    commit

--- Lab-2/Odd_or_Even.py
+++
@@ -1,7 +0,0 @@
-# Python Program to Check if a Number is Odd or Even
-
-num = int(input("Enter number:"))
-if (num % 2) == 0:
-    print("{0} is Even".format(num))
-else:
-    print("{0} is Odd".format(num))    
