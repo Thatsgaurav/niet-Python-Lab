ffca6b6990cbbb1d501510d589bcd13e771c2f8c
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-03-19 06:03:13 -0500

    commit

--- Problem Solving using Python Lab. Index/88.alphabet_words.py
+++ Problem Solving using Python Lab. Index/88.alphabet_words.py
@@ -1,2 +1,7 @@
 # WAP that prompts user to enter an alphabet and then print all the words that starts with that alphabet. 
 
+str1 = input("Enter any sentance :")
+alpha = input("Enter the alphabet :")
+print("Word starts with",alpha,"are :")
+for i in str1.split(alpha):
+    print(i,end=",")
