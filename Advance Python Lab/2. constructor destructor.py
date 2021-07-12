d0f0f18c26bfd83e4e0418eb0deac467f5448809
Gaurav Singh <0201csai117@niet.co.in>
2021-07-12 20:53:26 +0530

    commit

--- Advance Python Lab/2. constructor destructor.py
+++ Advance Python Lab/2. constructor destructor.py
@@ -1,6 +1,14 @@
 # 2. Write a program to implement default constructor, parameterized constructor, and destructor.
 
 # default constructor
+class Student:
+    branchcode = 117
+    fullname = "Gaurav Singh"
+    def info(self):
+        print("Bransh code: ",self.branchcode)
+        print("Student Name:", self.fullname)
+emp =Student()
+emp.info()        
 
 # parameterized constructor
 
